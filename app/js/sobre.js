16c184f7a7a43e94ac48ffde48d8ff2c6a6da6ff
Thiago Resende <thiago.gcresende@gmail.com>
2019-09-26 22:37:33 -0300

    sobre

--- app/js/sobre.js
+++ app/js/sobre.js
@@ -1,4 +1,12 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, shell } = require('electron');
 
 const linkSobre = document.getElementById('link-fechar');
+const linkGithub = document.getElementById('link-github')
+const versaoElectron = document.getElementById('versao-electron')
+
 linkSobre.addEventListener('click', () => ipcRenderer.send('fechar-janela-sobre'));
+linkGithub.addEventListener('click', () => shell.openExternal('https://github.com/tresende'));
+
+window.onload = function(){
+    versaoElectron.textContent = process.versions.electron;
+}
